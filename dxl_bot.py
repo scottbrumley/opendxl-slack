1cb7e0fa3f713943bd75531eb6c48a752b9f2b02
sbrumley <sbrumley@wireless-town.com>
2017-04-29 16:41:23 -0400

    Connecting bot to slack

---
+++ dxl_bot.py
@@ -0,0 +1,76 @@
+import os
+import time
+from slackclient import SlackClient
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def get_bot_id():
+    '''
+    Use the environment variable BOT_NAME to get the BOT ID from 
+    Slack and returns the BOT ID
+    '''
+    BOT_NAME = os.environ.get('BOT_NAME')
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                return user.get('id')
+
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command with numbers, delimited by spaces."
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    # starterbot's ID as an environment variable
+    BOT_ID = get_bot_id()
+
+    if BOT_ID == None:
+        print "BOT_ID is missing from ENV"
+        exit(0)
+
+    # constants
+    AT_BOT = "<@" + BOT_ID + ">"
+    EXAMPLE_COMMAND = "do"
+
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
