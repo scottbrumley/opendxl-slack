fd0c8d69a9c2856d9dd15f3ba7899add5e1dcb6f
sbrumley <sbrumley@wireless-town.com>
2017-04-30 03:31:40 -0400

    removed all functions redundant with opendxl-virtualbox project

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,131 +1,16 @@
 #!/usr/bin/env bash
 
-#set -e
-
-REQ_PY_VER="2.7.9"
-REQ_SSL_VER="1.0.1"
-
-if [[ -d "/vagrant" ]]; then
-    ROOT_DIR="/vagrant/"
-else
-    ROOT_DIR="$(pwd)/"
-fi
-
-installPythonEnv(){
-    wget https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh -P /tmp
-    sudo chmod +x /tmp/Anaconda2-4.3.1-Linux-x86_64.sh
-    /tmp/Anaconda2-4.3.1-Linux-x86_64.sh -b -p /home/vagrant/anaconda2
-    /home/vagrant/anaconda2/bin/conda create -y --name py2.7.9 python=2.7.9
-    source /home/vagrant/anaconda2/envs/py2.7.9/bin/activate py2.7.9
-}
-
-aptCleanUp(){
-    sudo apt-get -y autoremove
-}
-
-installGit(){
-    ### Install Git
-    sudo apt-get install -y git
-}
-
-installPip(){
-    ### Install Pip
-    echo "Installing Pip"
-    sudo apt-get install -y python-pip
-}
-
-installCommonPython(){
-    ### Install Common
-    echo "Install Common for Python"
-    sudo pip install common
-}
-installOpenDXLCLient(){
-    ### Install Open DXL Client
-    echo "Installing Open DXL Client"
-    cd ${ROOT_DIR}
-    sudo git clone https://github.com/opendxl/opendxl-bootstrap-python.git
-    cd ${ROOT_DIR}opendxl-bootstrap-python
-    sudo python setup.py install
-}
-
-checkOpenSSL(){
-### Check OpenSSL
-SSL_VER=$(python -c 'import ssl; print(ssl.OPENSSL_VERSION)')
-
-if [[ "${SSL_VER}" =~ "${REQ_SSL_VER}" ]]; then
-    echo "Already Version ${REQ_SSL_VER}"
-else
-    echo "Need OpenSSL version ${REQ_SSL_VER} or higher"
-fi
-}
-
-createDXLConfigDirs(){
-    ### Create Directories
-    sudo mkdir -p ${ROOT_DIR}brokercerts
-    sudo mkdir -p ${ROOT_DIR}certs
-    sudo touch ${ROOT_DIR}dxlclient.config
-}
-
-installDocker(){
-    sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    # Verify  sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
-    sudo gpasswd -a vagrant docker
-    sudo service docker restart
-}
-installFlask(){
-    ## Setup Flask
-    ## Use flask run --host=0.0.0.0 to start Flask
-    sudo pip install flask
-    sudo echo 'export FLASK_APP=$ROOT_DIR/examples/tie_rep_api.py' >> /etc/bash.bashrc
-}
-
-installDos2Unix(){
-    sudo apt-get install -y dos2unix
-}
+set -e
 
 installSlackClient(){
     sudo pip install --upgrade slackclient
-    #git clone https://github.com/slackapi/python-slackclient.git
-    #cd python-slackclient/
-    #sudo pip install --upgrade -r requirements.txt
-    #sudo python install setup
-
 }
 
 setEnvVariables(){
     /vagrant/scripts/env.sh
 }
 
-installWatson(){
-    sudo pip install --upgrade watson-developer-cloud
-}
-
-setupLogin(){
-    echo "#!/bin/bash" | sudo tee -a /etc/profile.d/environment.sh
-    echo "cd /vagrant" | sudo tee -a /etc/profile.d/environment.sh
-    #echo "source /home/vagrant/anaconda2/envs/py2.7.9/bin/activate" | sudo tee -a /etc/profile.d/environment.sh
-    sudo chmod +x /etc/profile.d/environment.sh
-}
 
 setEnvVariables
-##installPythonEnv
-##aptCleanUp
-#installGit
-#installPip
-##installCommonPython
-#installOpenDXLCLient
-##checkOpenSSL
-#installDos2Unix
-#installSlackClient
-##installWatson
-#setupLogin
+installSlackClient
 
-#if [[ "${ROOT_DIR}" == "/vagrant" ]]; then
-#    installDocker
-#fi
-##installFlask
