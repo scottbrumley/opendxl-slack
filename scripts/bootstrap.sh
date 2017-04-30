89a03f98f22896177b548d9d4031d8416c30d2d1
sbrumley <sbrumley@wireless-town.com>
2017-04-30 15:43:11 -0400

    Install DXL client and TIE client

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,7 +10,25 @@ setEnvVariables(){
     /vagrant/scripts/env.sh
 }
 
+function installOpenDXLClient {
+    ### Install Open DXL Client
+    echo "Installing Open DXL Client"
+    cd /vagrant
+    sudo git clone https://github.com/opendxl/opendxl-client-python.git
+    cd /vagrant/opendxl-client-python
+    sudo python setup.py install
+}
+
+function installOpenDXLTIEClient {
+    ### Install Open DXL TIE Client
+    echo "Installing Open DXL TIE Client"
+    cd /vagrant
+    sudo git clone https://github.com/opendxl/opendxl-tie-client-python.git
+    cd /vagrant/opendxl-tie-client-python
+    sudo python setup.py install
+}
 
 setEnvVariables
 installSlackClient
-
+installOpenDXLClient
+installOpenDXLTIEClient
