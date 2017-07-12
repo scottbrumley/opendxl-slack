32e8bf6c8848f3854b187b6384c6742632786b69
sbrumley <sbrumley@wireless-town.com>
2017-07-12 13:53:32 -0400

    decided to allow user to set git path manually

--- vssh.sh
+++ vssh.sh
@@ -2,7 +2,6 @@
 
 if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
-    set "PATH=%PATH%;%programfiles%\\Git\\usr\\bin"
 fi
 
 vagrant up --provider=virtualbox && vagrant ssh
