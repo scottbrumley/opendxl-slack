e3afe2bf4f26a2134255d33bab6a64be662fd428
sbrumley <sbrumley@wireless-town.com>
2017-07-12 13:48:56 -0400

    typo

--- vssh.sh
+++ vssh.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
-    sets "PATH=%PATH%;%programfiles%\\Git\\usr\\bin"
+    setx "PATH=%PATH%;%programfiles%\\Git\\usr\\bin"
 fi
 
 vagrant up --provider=virtualbox && vagrant ssh
