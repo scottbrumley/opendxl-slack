8eef0efad98903a0eb2f3c4a64c49dcd90b93f03
sbrumley <sbrumley@wireless-town.com>
2017-07-12 13:43:08 -0400

    escape chars

--- vssh.sh
+++ vssh.sh
@@ -2,7 +2,7 @@
 
 if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
-    set PATH=%PATH%;%programfiles%\Git\usr\bin
+    set PATH=%PATH%;%programfiles%\\Git\\usr\\bin
 fi
 
 vagrant up --provider=virtualbox && vagrant ssh
